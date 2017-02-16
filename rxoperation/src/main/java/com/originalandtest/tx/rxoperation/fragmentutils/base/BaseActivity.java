a12914b05209db427d616fe2e3be3c1326417572
tx <123436059@qq.com>
2017-02-16 13:52:07 +0800

    MD快速开发框架

---
+++ rxoperation/src/main/java/com/originalandtest/tx/rxoperation/fragmentutils/base/BaseActivity.java
@@ -0,0 +1,18 @@
+package com.originalandtest.tx.rxoperation.fragmentutils.base;
+
+import android.os.Bundle;
+import android.support.v4.app.SupportActivity;
+
+/**
+ * Created by Taxi on 2017/2/16.
+ */
+
+public class BaseActivity extends SupportActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
