3842a9b9843d9c11f50a861962a1badf4148aac2
tx <123436059@qq.com>
2017-02-18 08:54:24 +0800

    mainactivity

--- rxoperation/src/main/java/com/originalandtest/tx/rxoperation/fragmentutils/base/BaseActivity.java
+++ rxoperation/src/main/java/com/originalandtest/tx/rxoperation/fragmentutils/base/BaseActivity.java
@@ -1,18 +1,56 @@
 package com.originalandtest.tx.rxoperation.fragmentutils.base;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.SupportActivity;
+import android.view.WindowManager;
 
 /**
  * Created by Taxi on 2017/2/16.
  */
 
-public class BaseActivity extends SupportActivity {
+public abstract class BaseActivity extends SupportActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(bindLayout());
+
+        if (isSetTransparentBar()) {
+            if (Build.VERSION.SDK_INT >= 19) {
+                /*透明状态栏*/
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                /*透明导航栏*/
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            }
+        }
+        /*处理业务逻辑*/
+        doBusiness(savedInstanceState);
+
 
 
     }
+
+
+    /**
+     * 处理业务逻辑
+     *
+     * @param savedInstanceState
+     */
+    protected abstract void doBusiness(Bundle savedInstanceState);
+
+
+    /**
+     * 是否设置透明状态栏
+     *
+     * @return
+     */
+    protected abstract boolean isSetTransparentBar();
+
+    /**
+     * 绑定布局
+     *
+     * @return
+     */
+    protected abstract int bindLayout();
 }
